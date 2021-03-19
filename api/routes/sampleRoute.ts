9485f809f2bbad4b5fc0dbd47f2e6a1254a9d7a5
Nils Sutter <nils.sutter@door2door.io>
2021-03-19 18:12:41 +0000

    Adapt tsconfig and ignore generated compiled js code

--- api/routes/sampleRoute.ts
+++ api/routes/sampleRoute.ts
@@ -4,10 +4,10 @@ import { ExpressRouteFunc } from "../lib/types/expressRouteFunc";
 import { UseCase } from "../usecases/useCase";
 
 // To be able to use a Dependency Injection approach, all route functions must return a type of ExpressRouteFunc.
-// Express route functions only expect 3 arguments, req, res and next, 
-	// e.g. app.get('/some_route', (req, res, next)), so no dependency can be passed here.
-// To be able to pass any dependency, the ExpressRouteFunc will be 
-// wrapped in another function, so that they can dynamically pass in any dependency in app.ts and easier mock them out in unit tests.
+// General express route callbacks only expect 3 arguments, req, res and next, 
+	// e.g. app.get('/some_route', (req, res, next) => {...}), so no dependency can be passed here.
+// To be able to pass any dependency, the ExpressRouteFunc will be wrapped in another function, so that they can 
+// dynamically pass in any dependency in app.ts and easier mock them out in the routes unit tests.
 export const sampleRoute = (useCase: UseCase): ExpressRouteFunc => {
 	return async (_req: Request, res: Response) => {
 		respondWith(useCase.perform(), res)

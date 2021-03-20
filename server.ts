43f1651cf5272c2d87f915fa1c175c68e075c665
Nils Sutter <48183520+NilsSutter@users.noreply.github.com>
2021-03-20 23:49:06 +0100

    Setup mongodb (#1)
    Refactor service.ts file & setup mongodb

--- server.ts
+++ server.ts
@@ -1,9 +1,18 @@
 import app from "./app"
+import { DatabaseConnection } from "./db/databaseConnection";
 
 // Application entry
 const port: string = process.env.PORT || '8080';
 app.set('port', port);
 
-if (app.listen(port)) {
-  console.log(`App running on port ${port}`)
-}
+const db = DatabaseConnection.getInstance().establish()
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("Successfully connected to the database")
+
+  // start server
+  if (app.listen(port)) {
+    console.log(`App running on port ${port}`)
+  }
+});

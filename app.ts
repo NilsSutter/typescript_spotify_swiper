b83f78841f2bb2afb354b6f3abe9270fd2fa921b
Nils Sutter <nils.sutter@door2door.io>
2021-03-20 08:55:40 +0000

    Adapt start script and install cookie-parser

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { sampleRoute } from "./api/routes/sampleRoute"
-import { Failure, Success, Base } from "./api/usecases/base"
+import { SampleUseCase } from "./api/usecases/sampleUseCase"
 
 const app: express.Application = express()
 
@@ -9,12 +9,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // Dependencies
-class SampleUseCase extends Base {
-  perform(): Success | Failure {
-    return this.failure("Sample error")
-  }
-}
-
 const sampleUseCase = new SampleUseCase
 
 // v0 routes
